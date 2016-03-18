11ff18260a923df450028328c6c1a5ffa027e902
Nuno Faria <nunofaria11@gmail.com>
2016-03-18 16:22:25 +0000

    try onclick

--- sw.js
+++ sw.js
@@ -2,16 +2,16 @@
 console.log('inside service worker');
 
 self.onnotificationclick = function(event) {
-	var data = event.notification.data;
-	var pathname = data.pathname || '/';
-	//console.log('data:', event.notification.data);
-    event.notification.close();   
+    var data = event.notification.data;
+    var pathname = data.pathname || '/';
+    //console.log('data:', event.notification.data);
+    event.notification.close();
     // This looks to see if the current is already open and
     // focuses if it is
     event.waitUntil(clients.matchAll({
         type: "window"
     }).then(function(clientList) {
-
+        console.log('clientsList:', clientList)
         var url = pathname;
 
         for (var i = 0; i < clientList.length; i++) {
@@ -22,6 +22,9 @@ self.onnotificationclick = function(event) {
             }
 
         }
-        if (clients.openWindow) return clients.openWindow(url);
+        if (clients.openWindow) {
+        	console.log('opening window ', url);
+        	return clients.openWindow(url);
+        }
     }));
 };
